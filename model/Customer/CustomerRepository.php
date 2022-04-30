e3bd0347e88a12472671912bbf46d59ebf4f5751
Vinicius Ramos <vinicius.ramos@atmtec.com.br>
2022-04-30 19:09:49 -0300

    Correção de bugs

--- model/Customer/CustomerRepository.php
+++ model/Customer/CustomerRepository.php
@@ -15,12 +15,9 @@ class CustomerRepository
 
     public function get($customer_id, $contract_id)
     {
-        DB::enableQueryLog();
         return $this->customer->with(['contract' => function ($query) use ($contract_id) {
             $query->fields();
             $query->where('contract_id', $contract_id)->first();
         }, 'contract.customer'])->find($customer_id);
-        $query = DB::getQueryLog();
-        dd($query);
     }
 }
