4f39d09c29693e37371a7b6476eabe8b7b58836d
Vinicius Ramos <vinicius.ramos@atmtec.com.br>
2022-04-30 19:16:43 -0300

    Versão corrigido o campo do contract_id

--- resources/views/contract/show.blade.php
+++ resources/views/contract/show.blade.php
@@ -3,7 +3,7 @@
 
     <h1 class="display-3">Contrato</h1>
     <p>Eu <span class="font-weight-bold">{!! $customer->name!!}</span>, aceito o contrato tipo <span
-                class="font-weight-bold">{!! $customer->contract->contract_id !!}</span> sob as condições:</p>
+                class="font-weight-bold">{!! $customer->contract->contractId !!}</span> sob as condições:</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
         dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
         ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
